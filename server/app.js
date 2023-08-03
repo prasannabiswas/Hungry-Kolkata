5657d03b0080ccc095a830016264366961f9797e
Prasanna Biswas <probiswas82@gmail.com>
2023-08-03 10:56:23 +0530

    db added to api backend

--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+const backApi = require("./db.json");
+
+app.use(cors());
+
 
 app.get("/",(req,res)=>{
-    res.send("Hello world");
+    res.send("Hello");
 });
+
+app.get("/pizzas", (req,res)=>{
+    res.send(backApi.pizzas);
+});
+
 
 app.listen(3000, ()=>{
     console.log("Server sunning on port 3000");
